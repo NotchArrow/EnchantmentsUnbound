44831dd874d8f6b4f1dcf28580444ce01a8e4717
NotchArrow <149845992+NotchArrow@users.noreply.github.com>
2025-05-25 13:46:47 -0400

    v1.1.0

--- src/main/java/notcharrow/enchantmentsunbound/mixin/AnvilScreenHandlerMixin.java
+++ src/main/java/notcharrow/enchantmentsunbound/mixin/AnvilScreenHandlerMixin.java
@@ -66,15 +66,15 @@ public class AnvilScreenHandlerMixin {
 			ItemStack output = leftInput.copy();
 			for (Object2IntMap.Entry<RegistryEntry<Enchantment>> entry: outputEnchants.object2IntEntrySet()) {
 				RegistryEntry<Enchantment> enchantment = entry.getKey();
-				output.addEnchantment(enchantment, entry.getIntValue());
+				output.addEnchantment(enchantment, Math.min(entry.getIntValue(), serverHardCap(entry, enchantment)));
 			}
 			for (Object2IntMap.Entry<RegistryEntry<Enchantment>> entry: leftEnchants.object2IntEntrySet()) {
 				RegistryEntry<Enchantment> enchantment = entry.getKey();
-				output.addEnchantment(enchantment, entry.getIntValue());
+				output.addEnchantment(enchantment, Math.min(entry.getIntValue(), serverHardCap(entry, enchantment)));
 			}
 			for (Object2IntMap.Entry<RegistryEntry<Enchantment>> entry: rightEnchants.object2IntEntrySet()) {
 				RegistryEntry<Enchantment> enchantment = entry.getKey();
-				output.addEnchantment(enchantment, entry.getIntValue());
+				output.addEnchantment(enchantment, Math.min(entry.getIntValue(), serverHardCap(entry, enchantment)));
 			}
 
 			self.getSlot(2).setStack(output);
@@ -131,5 +131,52 @@ public class AnvilScreenHandlerMixin {
 			}
 		}
 		return overleveled;
+	}
+
+	@Unique
+	private static int serverHardCap(Object2IntMap.Entry<RegistryEntry<Enchantment>> entry, RegistryEntry<Enchantment> enchantment) {
+		String enchantmentID = enchantment.getIdAsString();
+		enchantmentID = enchantmentID.replace("minecraft:", "");
+		return switch (enchantmentID) {
+			case "aqua_affinity" -> ConfigManager.config.aqua_affinity;
+			case "bane_of_arthropods" -> ConfigManager.config.bane_of_arthropods;
+			case "binding_curse" -> ConfigManager.config.binding_curse;
+			case "blast_protection" -> ConfigManager.config.blast_protection;
+			case "channeling" -> ConfigManager.config.channeling;
+			case "depth_strider" -> ConfigManager.config.depth_strider;
+			case "efficiency" -> ConfigManager.config.efficiency;
+			case "feather_falling" -> ConfigManager.config.feather_falling;
+			case "fire_aspect" -> ConfigManager.config.fire_aspect;
+			case "fire_protection" -> ConfigManager.config.fire_protection;
+			case "flame" -> ConfigManager.config.flame;
+			case "fortune" -> ConfigManager.config.fortune;
+			case "frost_walker" -> ConfigManager.config.frost_walker;
+			case "impaling" -> ConfigManager.config.impaling;
+			case "infinity" -> ConfigManager.config.infinity;
+			case "knockback" -> ConfigManager.config.knockback;
+			case "looting" -> ConfigManager.config.looting;
+			case "loyalty" -> ConfigManager.config.loyalty;
+			case "luck_of_the_sea" -> ConfigManager.config.luck_of_the_sea;
+			case "lure" -> ConfigManager.config.lure;
+			case "mending" -> ConfigManager.config.mending;
+			case "multishot" -> ConfigManager.config.multishot;
+			case "piercing" -> ConfigManager.config.piercing;
+			case "power" -> ConfigManager.config.power;
+			case "projectile_protection" -> ConfigManager.config.projectile_protection;
+			case "protection" -> ConfigManager.config.protection;
+			case "punch" -> ConfigManager.config.punch;
+			case "quick_charge" -> ConfigManager.config.quick_charge;
+			case "respiration" -> ConfigManager.config.respiration;
+			case "riptide" -> ConfigManager.config.riptide;
+			case "sharpness" -> ConfigManager.config.sharpness;
+			case "silk_touch" -> ConfigManager.config.silk_touch;
+			case "smite" -> ConfigManager.config.smite;
+			case "soul_speed" -> ConfigManager.config.soul_speed;
+			case "sweeping" -> ConfigManager.config.sweeping;
+			case "thorns" -> ConfigManager.config.thorns;
+			case "unbreaking" -> ConfigManager.config.unbreaking;
+			case "vanishing_curse" -> ConfigManager.config.vanishing_curse;
+			default -> entry.getKey().value().getMaxLevel();
+		};
 	}
 }
