90615adacd89fc4f2f150023b74c172a5d4688c1
NotchArrow <149845992+NotchArrow@users.noreply.github.com>
2025-12-05 13:50:18 -0500

    v4.1.1

--- src/main/java/com/notcharrow/enchantmentsunbound/mixin/EnchantmentMixin.java
+++ src/main/java/com/notcharrow/enchantmentsunbound/mixin/EnchantmentMixin.java
@@ -5,7 +5,6 @@ import net.minecraft.enchantment.Enchantment;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.entry.RegistryEntry;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -13,17 +12,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(Enchantment.class)
 public abstract class EnchantmentMixin {
 
-	/**
-	 * @author NotchArrow
-	 * @reason Allow enchantment combos like Mending + Infinity
-	 */
-	@Overwrite
-	public static boolean canBeCombined(RegistryEntry<Enchantment> first, RegistryEntry<Enchantment> second) {
-		return true;
+	@Inject(method = "canBeCombined", at = @At("TAIL"), cancellable = true)
+	private static void canBeCombined(RegistryEntry<Enchantment> first, RegistryEntry<Enchantment> second, CallbackInfoReturnable<Boolean> cir) {
+		cir.setReturnValue(true);
 	}
 
 	@Inject(method = "isAcceptableItem", at = @At("TAIL"), cancellable = true)
-	public void isAcceptableItem(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
+	private void isAcceptableItem(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
 		if (!ConfigManager.config.itemEnchantConflicts) {
 			cir.setReturnValue(true);
 		}
