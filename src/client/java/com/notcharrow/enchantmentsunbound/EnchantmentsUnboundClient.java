f96b2ee6f03fb39766ace4222ff97fc42df27b54
NotchArrow <149845992+NotchArrow@users.noreply.github.com>
2026-03-21 21:23:00 -0400

    v4.3.0 fixes

--- src/client/java/com/notcharrow/enchantmentsunbound/EnchantmentsUnboundClient.java
+++ src/client/java/com/notcharrow/enchantmentsunbound/EnchantmentsUnboundClient.java
@@ -1,8 +1,8 @@
 package com.notcharrow.enchantmentsunbound;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.resource.v1.ResourceLoader;
-import net.fabricmc.fabric.api.resource.v1.pack.PackActivationType;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
@@ -14,11 +14,11 @@ public class EnchantmentsUnboundClient implements ClientModInitializer {
 	public void onInitializeClient() {
 		Style textStyle = Style.EMPTY.withColor(Formatting.DARK_PURPLE).withBold(true);
 
-		ResourceLoader.registerBuiltinPack(
+		ResourceManagerHelper.registerBuiltinResourcePack(
 				Identifier.of("enchantments-unbound", "enchantment_numbers"),
 				FabricLoader.getInstance().getModContainer("enchantments-unbound").orElseThrow(),
 				Text.literal("Enchantment Numbers").setStyle(textStyle),
-				PackActivationType.NORMAL
+				ResourcePackActivationType.NORMAL
 		);
 	}
 }
